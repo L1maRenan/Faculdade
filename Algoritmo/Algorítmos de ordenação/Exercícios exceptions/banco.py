a561dc7dbf4d574c06e9e8a653a852df4daec8c2
L1maRenan <lrenan014@gmail.com>
2024-10-27 15:26:23 -0300

    Delete Algoritmo/Algorítmos de ordenação/Exercícios exceptions directory

--- Algoritmo/Algorítmos de ordenação/Exercícios exceptions/banco.py
+++
@@ -1,8 +0,0 @@
-saldo = float(input("Digite o saldo da conta"))
-transferencia = float(input("Digite o valor da transferência"))
-try:
-    if saldo < transferencia:
-        raise ValueError
-    
-except ValueError:
-    print("Valor da transferência excede o saldo.")
